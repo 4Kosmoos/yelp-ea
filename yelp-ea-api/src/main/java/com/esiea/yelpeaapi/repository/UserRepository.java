897c01c6c66d6a6c6ea9bf5722281c750233b9f1
ingivre <besseau@et.esiea.fr>
2025-03-26 10:56:39 +0100

    Remove unused method findByNom from UserRepository

--- yelp-ea-api/src/main/java/com/esiea/yelpeaapi/repository/UserRepository.java
+++ yelp-ea-api/src/main/java/com/esiea/yelpeaapi/repository/UserRepository.java
@@ -7,7 +7,4 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    List<User> findByNom(String nom);
-
-
 }
