adaf83dc247adc1d8a4140878dee245922fb1c6a
4Kosmoos <firmin.shu@gmail.com>
2025-03-26 09:45:07 +0100

    workInProgress

--- yelp-ea-api/src/main/java/com/esiea/yelpeaapi/repository/RestaurantRepository.java
+++ yelp-ea-api/src/main/java/com/esiea/yelpeaapi/repository/RestaurantRepository.java
@@ -4,7 +4,9 @@ import com.esiea.yelpeaapi.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
-    List<Restaurant> findByNom(String nom);
+    List<Restaurant> getAll();
 }
