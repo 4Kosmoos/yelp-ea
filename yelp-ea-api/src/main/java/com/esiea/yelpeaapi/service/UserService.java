5722574af27791a01b79b5394bb80aa0ff48cde9
ingivre <besseau@et.esiea.fr>
2025-03-26 10:41:37 +0100

    Remove unused import in UserService

--- yelp-ea-api/src/main/java/com/esiea/yelpeaapi/service/UserService.java
+++ yelp-ea-api/src/main/java/com/esiea/yelpeaapi/service/UserService.java
@@ -4,8 +4,6 @@ import com.esiea.yelpeaapi.entity.User;
 import com.esiea.yelpeaapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
